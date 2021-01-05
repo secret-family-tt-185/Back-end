98d343640518f72fce0f295277e5cf9229ab835d
Matthew Martin <zhintateo19924559@gmail.com>
2021-01-04 19:42:10 -0600

    Another correction

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const db = require('../users/users-model.js');
