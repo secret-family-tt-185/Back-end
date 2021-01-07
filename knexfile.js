d73eb1d835dfa259d0195358760c832b2ed77fab
Matthew Martin <zhintateo19924559@gmail.com>
2021-01-06 23:42:55 -0600

    Added Pool to knexfile

--- knexfile.js
+++ knexfile.js
@@ -25,6 +25,10 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     //searchPath: ['knex', 'public'],
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       directory: './data/migrations',
     },
